59defd69d17ec47eab8b1ce0c35323951ea2cde2
taducduy123 <taducduy2003@gmail.com>
2024-08-22 23:56:47 +0700

    update database

--- db.sql
+++ db.sql
@@ -1,85 +1,207 @@
-CREATE database library;
-use library;
-
-drop database library;
-
-create table authors
-(
-    authorId   int auto_increment primary key,
-    authorName nvarchar(30),
-    isDelete   bool default false
-);
 
 
-create table categories
-(
-    categoryId   int primary key auto_increment,
-    categoryName nvarchar(30)
-);
+CREATE DATABASE  IF NOT EXISTS `library` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `library`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: library
+-- ------------------------------------------------------
+-- Server version	8.2.0
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `authors`
+--
 
-create table books
-(
-    bookId      nvarchar(30) primary key,
-    authorId    nvarchar(30),
-    categoryId  int,
-    bookName    nvarchar(100),
-    quantity    int,
-    publishDate date,
-    isDelete    bool default false
-);
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `authors` (
+  `authorId` int NOT NULL AUTO_INCREMENT,
+  `authorName` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `isDelete` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`authorId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table readers
-(
-    readerId          nvarchar(30) primary key,
-    readerName        nvarchar(30),
-    readerEmail       nvarchar(30),
-    readerPhoneNumber nvarchar(11),
-    readerDob         date,
-    address           nvarchar(100),
-    isDelete          bool default false
-);
+--
+-- Dumping data for table `authors`
+--
 
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+INSERT INTO `authors` VALUES (1,'Nguyễn Nhật Ánh',0),(2,'Tô Hoài',0),(3,'Nguyễn Du',0),(4,'Nam Cao',0),(5,'Xuân Quỳnh',0),(8,'Charlotte Bronte',0),(9,'George Orwell',0),(10,'new author',0);
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
 
-create table borrow
-(
-    borrowId   int auto_increment primary key,
-    bookId     nvarchar(30),
-    readerId   nvarchar(30),
-    borrowDate date,
-    returnDate date,
-    dueDate date,
-    isDelete   bool default false
-);
+--
+-- Table structure for table `books`
+--
 
-create table users
-(
-    userId   int auto_increment primary key,
-    username varchar(100),
-    password varchar(1000)
-);
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `bookId` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `authorId` int DEFAULT NULL,
+  `categoryId` int DEFAULT NULL,
+  `bookName` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `publishDate` date DEFAULT NULL,
+  `isDelete` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`bookId`),
+  KEY `fk_authorId` (`authorId`),
+  KEY `fk_categoryId` (`categoryId`),
+  CONSTRAINT `fk_authorId` FOREIGN KEY (`authorId`) REFERENCES `authors` (`authorId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_categoryId` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`categoryId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `books`
+--
 
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES ('B16542',10,9,'Book01',10,'2024-08-14',0),('B1TEDV',1,2,'Kính Vạn Hoa',10,'1995-01-01',0),('B33FSB',9,4,'Pride and Prejudice',20,'1941-01-01',0),('B3JFFD',3,3,'Truyện Kiều',14,'1820-01-01',0),('B4GDEV',8,4,'1984',4,'2024-08-05',0),('BFHDBC',2,2,'Dế Mèn Phiêu Lưu Ký',4,'1941-01-01',0),('BNNNN',1,1,'No Book',0,'2024-08-22',0);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE books
-    ADD CONSTRAINT fk_authorId
-        FOREIGN KEY (authorId)
-            REFERENCES authors (authorId);
+--
+-- Table structure for table `borrow`
+--
 
-ALTER TABLE books
-    ADD CONSTRAINT fk_categoryId
-        FOREIGN KEY (categoryId)
-            REFERENCES categories (categoryId);
+DROP TABLE IF EXISTS `borrow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `borrow` (
+  `borrowId` int NOT NULL AUTO_INCREMENT,
+  `bookId` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `readerId` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `borrowDate` date DEFAULT NULL,
+  `returnDate` date DEFAULT NULL,
+  `dueDate` date DEFAULT NULL COMMENT 'ngay tra',
+  `isDelete` tinyint(1) DEFAULT '0',
+  `status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`borrowId`),
+  KEY `fk_bookId` (`bookId`),
+  KEY `fk_readerId` (`readerId`),
+  CONSTRAINT `fk_bookId` FOREIGN KEY (`bookId`) REFERENCES `books` (`bookId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_readerId` FOREIGN KEY (`readerId`) REFERENCES `readers` (`readerId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE borrow
-    ADD CONSTRAINT fk_bookId
-        FOREIGN KEY (bookId)
-            REFERENCES books (bookId);
+--
+-- Dumping data for table `borrow`
+--
 
-ALTER TABLE borrow
-    ADD CONSTRAINT fk_readerId
-        FOREIGN KEY (readerId)
-            REFERENCES readers (readerId);
+LOCK TABLES `borrow` WRITE;
+/*!40000 ALTER TABLE `borrow` DISABLE KEYS */;
+INSERT INTO `borrow` VALUES (74,'BFHDBC','R9A4DE','2024-08-22','2024-08-10',NULL,0,NULL),(75,'B4GDEV','R9A4DE','2024-08-22','2024-08-10',NULL,0,NULL),(76,'B3JFFD','R71D12','2024-08-22','2024-08-27',NULL,0,NULL),(77,'B33FSB','R71D12','2024-08-22','2024-08-27','2024-08-22',0,NULL),(78,'B4GDEV','R71D12','2024-08-22','2024-08-27','2024-08-22',0,NULL),(79,'B16542','R71D12','2024-08-22','2024-08-29',NULL,0,'REQUEST');
+/*!40000 ALTER TABLE `borrow` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `categories`
+--
 
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `categoryId` int NOT NULL AUTO_INCREMENT,
+  `categoryName` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Văn học'),(2,'Thiếu nhi'),(3,'Cổ điển'),(4,'Truyện ngắn'),(5,'Thơ'),(6,'Thần thoại'),(8,'Văn học hiện thực'),(9,'new category');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `readers`
+--
+
+DROP TABLE IF EXISTS `readers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `readers` (
+  `readerId` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
+  `readerName` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `readerEmail` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `readerPhoneNumber` varchar(11) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `readerDob` date DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci DEFAULT NULL,
+  `isBlock` tinyint(1) DEFAULT '0',
+  `userId` int DEFAULT NULL,
+  PRIMARY KEY (`readerId`),
+  KEY `readers_users_userId_fk` (`userId`),
+  CONSTRAINT `readers_users_userId_fk` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `readers`
+--
+
+LOCK TABLES `readers` WRITE;
+/*!40000 ALTER TABLE `readers` DISABLE KEYS */;
+INSERT INTO `readers` VALUES ('R71D12','Normal Reader','taducduy2003@gmail.com','0948448484','2024-08-14','Ha Noi',0,25),
+                             ('R9A4DE','Reader Late','hvjexxx@gmail.com','0948448483','2024-08-14','Ha Noi',0,24),
+                             ('RHDB32','Librarian','gvstudent2003@gmail.com','0948447372','2024-08-22','Ha Noi',0,21);
+/*!40000 ALTER TABLE `readers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userId` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(1000) DEFAULT NULL,
+  `role` varchar(10) DEFAULT 'reader',
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (21,'librarian','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','librarian'),(24,'latereader','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','reader'),(25,'normalreader','6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b','reader');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-08-22 23:23:25
